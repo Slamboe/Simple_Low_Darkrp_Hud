eea79a1e243e956f30c2d52fbc5b78d495735686
Slamboe <108998296+Slamboe@users.noreply.github.com>
2023-08-16 18:30:13 +0200

    Delete lua directory

--- lua/sdrphud.lua
+++
@@ -1,4 +0,0 @@
-include("sdrphud/cl_sdrpallpanels.lua")
-include("sdrphud/cl_nlrallpanels.lua")
-include("sdrphud/cl_scarmenu.lua")
-include("sdrphud/sdrpconfig.lua")
--- lua/sdrphud/sdrpconfig.lua
+++
@@ -1,7 +0,0 @@
-CONFIG_LANGUAGE_sdrphud = "dk" -- dk, en
-
-SERVERNAME = "YOUR SERVER NAME"
-
-SpeedMode = "kmh" -- Set it to "kmh" or "mph". "Nil" is not selected
-
-Vcmod = "true" -- Set it to true or false
